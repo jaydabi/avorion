8a0fa593dc0a260f989bece8b3e34654f023e8f9
Jonas Manusch <jonas@mymailway.de>
2020-03-08 20:34:00 +0100

    Update run_galaxy.sh

--- run_galaxy.sh
+++ run_galaxy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+GALAXY_NAME=${AVORION_GALAXY_NAME:=default}
+
+if [[ -z AVORION_ADMIN_STEAM_ID ]]; then
+  ADMIN_STEAM_ID=""
+else
+  ADMIN_STEAM_ID="--admin ${AVORION_GALAXY_NAME}"
+fi
+
 # run_galaxy.sh
-./server.sh --galaxy-name default --datapath ~/data/avorion/default
+./server.sh --galaxy-name "${GALAXY_NAME}" --datapath "~/data/avorion/${GALAXY_NAME}" $ADMIN_STEAM_ID
 
 exit 0
